9dd3a14f6783b5690061d2d42ce72b6d55e0d103
mas454 <masahiro454@gmail.com>
2016-07-28 17:51:00 +0900

    関数追加

--- instruction.scm
+++ instruction.scm
@@ -37,11 +37,61 @@
     (parse-modrm emu modrm)
     (set-r32 emu modrm (get-rm32 emu modrm))))
 
+(define (add-rm32-r32 emu)
+  (eip-add emu 1)
+  (let ([modrm (make <modrm>)])
+    (parse-modrm emu modrm)
+    (let* ([r32 (get-r32 emu modrm)]
+	   [rm32 (get-rm32 emu modrm)])
+      (set-rm32 emu modrm (+ rm32 r32)))))
+
+(define (sub-rm32-imm8 emu modrm)
+  (let* ([rm32 (get-rm32 emu modrm)]
+	 [imm8 (get-code8 emu 0)])
+    (eip-add emu 1)
+    (set-rm32 emu modrm (- rm32 imm8))))
+
+(define (code-83 emu)
+  (eip-add emu 1)
+  (let ([modrm (make <modrm>)])
+    (parse-modrm emu modrm)
+    (cond [(= (ref modrm opecode) 5)
+	   (sub-rm32-imm8 emu modrm)]
+	  [else
+	    (error (string-append "not implemented: 83 "
+				  (number->string (ref modrm opecode))
+				  "\n"))])))
+
+(define (inc-rm32 emu modrm)
+  (set-rm32 emu modrm (+ (get-rm32 emu modrm) 1)))
+
+
+
+(define (code-ff emu)
+  (eip-add emu 1)
+  (let ([modrm (make <modrm>)])
+    (parse-modrm emu modrm)
+    (cond [(= (ref modrm opecode) 0)
+	   (inc-rm32 emu modrm)]
+	  [else 
+	    (error (string-append "not implemented: FF "
+				  (number->string (ref modrm opecode))
+				  "\n"))]
+	  )
+    ))
+
 (define (get-instructions code)
-  (cond [(and (<= #xb8 code) (<= code (+ #xb8 7)))
+  (cond 
+    [(= code #x01) add-rm32-r32]
+    [(= code #x83) code-83]
+    [(= code #x89) mov-rm32-r32]
+    [(= code #x8b) mov-r32-rm32]
+    [(and (<= #xb8 code) (<= code (+ #xb8 7)))
 	 mov-r32-imm32]
-	[(= code #xeb) short-jump]
-	[(= code #xe9) near-jump]
-	[else '()]))
+    [(= code #xc7) mov-rm32-imm32]
+    [(= code #xeb) short-jump]
+    [(= code #xe9) near-jump]
+    [(= code #xff) code-ff]
+    [else '()]))
 
 (provide "instruction")
